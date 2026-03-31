4ede556817246b796a3c2806c3bb30f4873ad464
siontext <topaegr3525@kakao.com>
2026-03-31 23:59:38 +0900

    feat: docker compose와 볼륨마운트 설정 추가, 스프링 부트와 PostgreSQL DB연결

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,8 +1,12 @@
 plugins {
     alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.kotlin.jpa)        // JPA 엔티티용 no-arg 생성자 자동 생성
 }
 
 // infrastructure는 domain만 의존한다 (application, presentation은 볼 수 없다)
 dependencies {
     implementation(project(":domain"))
+
+    implementation(libs.spring.boot.starter.data.jpa)
+    runtimeOnly(libs.postgresql)
 }
