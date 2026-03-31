f14d6289cdab90090650022fdc836a1ccce7e68c
Jdragon <lee411806@naver.com>
2026-03-31 23:14:35 +0900

    fix: magic string 제거

--- oop-boot/src/main/kotlin/com/jaeyong/oop/boot/config/WebConfig.kt
+++ oop-boot/src/main/kotlin/com/jaeyong/oop/boot/config/WebConfig.kt
@@ -1,6 +1,7 @@
 package com.jaeyong.oop.boot.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -10,7 +11,14 @@ class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOrigins("http://localhost:3000", "http://localhost:8080") // 필요에 따라 추가
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowedMethods(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name(),
+                HttpMethod.PATCH.name(),
+                HttpMethod.OPTIONS.name()
+            )
             .allowedHeaders("*")
             .allowCredentials(true)
             .maxAge(3600)
